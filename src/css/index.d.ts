07441af85792d93ec762d15c2165bac2c54254f1
Tao Cumplido <taocumplido@gmail.com>
2021-03-26 06:21:03 +0100

    add missing exports

--- src/css/index.d.ts
+++ src/css/index.d.ts
@@ -1,5 +1,6 @@
 export * from './animations';
 export * from './box-alignment';
+export * from './compatibility';
 export * from './easing';
 export * from './object-model';
 export * from './properties';
